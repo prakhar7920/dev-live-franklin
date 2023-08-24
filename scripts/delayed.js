7e17d373055845162392c77cd7c5b01ebb0a8fff
prakharpathak <prakhar.pathak@nagarro.com>
2023-08-24 18:15:01 +0530

    slick changes

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -8,7 +8,3 @@ sampleRUM('cwv');
 var script1 = document.createElement('script');
 script1.src = 'https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js';
 document.getElementsByTagName('head')[0].appendChild(script1);
-
-var script = document.createElement('script');
-script.src = 'https://code.jquery.com/jquery-3.6.3.min.js'; // Check https://jquery.com/ for the current version
-document.getElementsByTagName('head')[0].appendChild(script);
